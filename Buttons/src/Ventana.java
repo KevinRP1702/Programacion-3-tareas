8e2c77cb2ff562abd7f6ad90753c0602563fb0c5
krip1 <krip1@DESKTOP-IGIEUAQ>
2024-03-13 19:51:41 -0600

    Unidad 3 - Ejercicio 7

---
+++ Buttons/src/Ventana.java
@@ -0,0 +1,77 @@
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Random;
+
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.LineBorder;
+
+public class Ventana extends JFrame{
+public Ventana() {
+
+			//Atributos de la ventana
+			this.setVisible(true);
+			this.setSize(500,750);
+			this.setLocation(200,200);
+			this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			this.setResizable(true);
+			this.setTitle("Click me");
+			this.setMinimumSize(new Dimension(250,250));
+			this.setMaximumSize(new Dimension(1000,750));
+			this.setLocationRelativeTo(null);
+
+			this.setLayout(null);
+
+			this.IniciarComponentes();
+		}
+
+	//Metodo para los componentes
+	public void IniciarComponentes() {
+
+		this.botones();
+	}
+	
+	public void botones() {
+		
+		//Primer panel
+		JPanel pnlBotones = new JPanel();
+		pnlBotones.setSize(this.getWidth(), this.getHeight());
+		pnlBotones.setBackground(Color.cyan);
+		pnlBotones.setLayout(null);
+		this.add(pnlBotones);
+		
+		 JButton btnClickMe = new JButton("Click me");
+	        btnClickMe.setBounds(150, 600, 170, 50);
+	        btnClickMe.setBackground(Color.decode("#DCCA8A"));
+	        btnClickMe.setFont(new Font("Arial", Font.BOLD, 20));
+	        pnlBotones.add(btnClickMe);
+	        btnClickMe.addActionListener(new ActionListener() {
+	            @Override
+	            public void actionPerformed(ActionEvent e) {
+	                int x = (int) Math.floor(Math.random() * 450 + 1);
+	                int y = (int) Math.floor(Math.random() * 650 + 1);
+	                int w = (int) Math.floor(Math.random() * 120 + 1);
+	                int h = (int) Math.floor(Math.random() * 120 + 1);
+	                Random rand = new Random();
+	                float r = rand.nextFloat();
+	                float g = rand.nextFloat();
+	                float b = rand.nextFloat();
+	                JButton otroBtn = new JButton(r + "," + g + "," + b);
+	                otroBtn.setBounds(x, y, w, h);
+	                otroBtn.setBackground(new Color(r, g, b));
+	               
+	                pnlBotones.add(otroBtn);
+	                getContentPane().repaint();
+	                getContentPane().revalidate();
+	            }
+	        });
+	}
+}
